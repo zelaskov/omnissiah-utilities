132aeb2537ce8d2e114b589e4328c748b4775ad4
zelaskov <marcin.zelasko@codetain.eu>
2022-04-28 22:17:35 +0200

    feat: revert repo structure

--- omnissiah-utilities/__init__.py
+++
@@ -1,5 +0,0 @@
-from helpers.open_projects import list_repositories, open_project
-from helpers.setup_local import install_packages
-from helpers.docker import cleanup_images
-from helpers.pull_repos import download_repos
-from main import choices
